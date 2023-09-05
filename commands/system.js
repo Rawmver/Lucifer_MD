4a916a3eec2ad536b46a4c7dd694d053b7adfab3
Rawmver <139946633+Rawmver@users.noreply.github.com>
2023-09-05 14:22:54 +0500

    system.js

--- commands/system.js
+++ commands/system.js
@@ -57,7 +57,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Sends Qr code to scan and get your session id."
          },
-         async(Suhail, msg, text) => {
+         async(Lucifer, msg, text) => {
              if (!text) return msg.reply(`*Provide me Text To Get QR*`);
              let h =`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${text}`;
              let buttonMessaged = 
@@ -67,7 +67,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
                  footer: ` Session bY >> ${Config.caption}`,
                  headerType: 4,
              };
-             return await Suhail.bot.sendMessage(msg.chat, buttonMessaged );
+             return await Lucifer.bot.sendMessage(msg.chat, buttonMessaged );
  
      })
          
@@ -79,7 +79,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
      filename: __filename,
      desc: "Remove image Background."
  },
- async(Suhail, msg, text) => {
+ async(Lucifer, msg, text) => {
    const _0x16b89c=_0x49c2;function _0x5e60(){const _0x33515f=['log','post','unlinkSync','send','caption','reply','524478FLbaqg','671116swmKkp','Uhh\x20Please,\x20Reply\x20To\x20An\x20Image/Video','1030BtYSxu','https://api.remove.bg/v1.0/removebg','187699OYwNUd','93599mPRWgy','auto','data','3341514nVKyCN','225184Osvztc','downloadAndSaveMediaMessage','catch','imageMessage','*_Uhh\x20Dear,\x20Please\x20provide\x20Valid\x20RemoveBg\x20Key_*\x0a\x0a\x20_Get\x20RemoveBg\x20api\x20key\x20from\x20remove.bg_\x0a\x20_then\x20Put\x20it\x20in\x20var\x20\x22REMOVE_BG_KEY\x22_\x0a\x0a','25NTBVPR','sending\x20removebg\x20image...!','14IYDGua','4141704bMSerR','REMOVE_BG_KEY','quoted','477ZxuHEt'];_0x5e60=function(){return _0x33515f;};return _0x5e60();}(function(_0x3045c0,_0x56c7b6){const _0x146dc7=_0x49c2,_0x492ef5=_0x3045c0();while(!![]){try{const _0x1f6d31=-parseInt(_0x146dc7(0xf0))/0x1*(-parseInt(_0x146dc7(0xe0))/0x2)+parseInt(_0x146dc7(0xd8))/0x3+-parseInt(_0x146dc7(0xec))/0x4+-parseInt(_0x146dc7(0xde))/0x5*(-parseInt(_0x146dc7(0xeb))/0x6)+parseInt(_0x146dc7(0xe1))/0x7+-parseInt(_0x146dc7(0xd9))/0x8*(parseInt(_0x146dc7(0xe4))/0x9)+parseInt(_0x146dc7(0xee))/0xa*(-parseInt(_0x146dc7(0xd5))/0xb);if(_0x1f6d31===_0x56c7b6)break;else _0x492ef5['push'](_0x492ef5['shift']());}catch(_0x1a9a50){_0x492ef5['push'](_0x492ef5['shift']());}}}(_0x5e60,0xe0b62));if(!msg[_0x16b89c(0xe3)])return await msg[_0x16b89c(0xea)]('*Reply\x20Any\x20Image\x20To\x20Remove\x20Background*');let mime=msg[_0x16b89c(0xe3)]['mtype'];if(mime!=_0x16b89c(0xdc))return await msg[_0x16b89c(0xea)](_0x16b89c(0xed));let media=await Suhail.bot[_0x16b89c(0xda)](msg[_0x16b89c(0xe3)]),anu=await TelegraPh(media);try{await fs[_0x16b89c(0xe7)](media);}catch(_0x14f72d){}const formData={'image_url':anu,'size':_0x16b89c(0xd6)};function _0x49c2(_0x16a55d,_0x16a672){const _0x5e6049=_0x5e60();return _0x49c2=function(_0x49c2f0,_0x45b252){_0x49c2f0=_0x49c2f0-0xd5;let _0x43a2e2=_0x5e6049[_0x49c2f0];return _0x43a2e2;},_0x49c2(_0x16a55d,_0x16a672);}axios[_0x16b89c(0xe6)](_0x16b89c(0xef),formData,{'headers':{'X-Api-Key':Config[_0x16b89c(0xe2)]},'responseType':'arraybuffer'})['then'](_0x466f9f=>{const _0x441490=_0x16b89c;console[_0x441490(0xe5)](_0x441490(0xdf));const _0x5b34e5=Buffer['from'](_0x466f9f[_0x441490(0xd7)],'binary');return msg[_0x441490(0xe8)](_0x5b34e5,{'caption':Config[_0x441490(0xe9)]},'image');})[_0x16b89c(0xdb)](_0x46980f=>{const _0x3d5c75=_0x16b89c;return msg[_0x3d5c75(0xe8)](_0x3d5c75(0xdd)+Config[_0x3d5c75(0xe9)]);});
  
      
@@ -94,7 +94,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "image to url."
          },
-         async(Suhail, msg, text) => {
+         async(Lucifer, msg, text) => {
              if (!msg.quoted) return await msg.reply(`*Reply To Any Image/Video To Get Url*`)
              let mime = msg.quoted.mtype
              if(mime !='videoMessage' && mime !='imageMessage' ) return await msg.reply("Uhh Please, Reply To An Image/Video")
@@ -112,7 +112,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Translate\'s given text in desird language."
          },
-         async(Suhail, msg, text) => {
+         async(Lucifer, msg, text) => {
              if(!text && !msg.quoted) return await msg.reply(`*Please Give Me Text. Example: _${prefix}trt en Who are you_*`);
              const translatte = require("translatte");
              let lang = text ? text.split(" ")[0].toLowerCase() : 'en';
@@ -132,7 +132,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Runs command in Heroku(server) shell."
          },
-         async(Suhail, msg, text,{ isCreator }) => {
+         async(Lucifer, msg, text,{ isCreator }) => {
               if (!isCreator) return msg.reply(tlang().owner)
               if(!text) return msg.reply("*Uhh PLease, Provide A Command to Run Heroku*")
               const { exec } = require("child_process")
@@ -171,7 +171,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Deletes note from db."
          },
-         async(Suhail, msg, text,{ isCreator }) => {
+         async(Lucifer, msg, text,{ isCreator }) => {
              const { tlang } = require('../lib/scraper')
              if (!isCreator) return msg.reply(tlang().owner)
              if(!text) return msg.reply("*Uhh PLease, Provide A Note Id. Ex .delnote 1*")
@@ -189,7 +189,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Deletes all notes from db."
          },
-         async(Suhail, msg, text, isCreator) => {
+         async(Lucifer, msg, text, isCreator) => {
              const { tlang } = require('../lib/scraper')
              if (!isCreator) return msg.reply(tlang().owner)
  
@@ -208,13 +208,13 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Bans user from using bot."
          },
-         async(Suhail, msg, text,{ isCreator}) => {
+         async(Lucifer, msg, text,{ isCreator}) => {
              if (!isCreator) return msg.reply(tlang().owner)
              try 
              {
                  let users = msg.quoted ? msg.quoted.sender : msg.mentionedJid ? msg.mentionedJid[0] : msg.msg.contextInfo.participant || false;
                  if (!users) return msg.reply(`❌ Please mention any user ${tlang().greet}.`)
-                 let pushnamer = Suhail.bot.getName(users);
+                 let pushnamer = Lucifer.bot.getName(users);
                  sck1.findOne({ id: users }).then(async(usr) => 
                  {
                      if (!usr) 
@@ -237,13 +237,13 @@ CURRENTLY RUNNING ON BETA VERSION!!
              filename: __filename,
              desc: "Unbans banned user (from using bot)."
          },
-         async(Suhail, msg, text,{ isCreator }) => {
+         async(Lucifer, msg, text,{ isCreator }) => {
              if (!isCreator) return msg.reply(`This command is only for my Owner`)
              try 
              {
                  let users = msg.quoted ? msg.quoted.sender : msg.mentionedJid ? msg.mentionedJid[0] : msg.msg.contextInfo.participant || false;
                  if (!users) return msg.reply("Please mention any user.❌")
-                 let pushnamer = Suhail.bot.getName(users);
+                 let pushnamer = Lucifer.bot.getName(users);
                  sck1.findOne({ id: users }).then(async(usr) =>{ // console.log(usr.ban);
                      if (!usr) { return msg.reply(`${pushnamer} is unbanned.`);}
                      if (usr.ban !== "true") return await msg.reply(`${usr.name} is already unbanned.`);
@@ -263,7 +263,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
              desc: "Adds a note on db.",
              filename: __filename
          },
-         async(Suhail, msg, text,{ isCreator }) => {
+         async(Lucifer, msg, text,{ isCreator }) => {
              if (!isCreator) return msg.reply(tlang().owner)
              if (!text) return msg.reply(`🔍 *Please Provide Text To Save In Notes*`)
   
@@ -281,7 +281,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
          filename: __filename,
          desc: "Shows list of all notes."
      },
-     async(Suhail, msg, text,{ isCreator }) => {
+     async(Lucifer, msg, text,{ isCreator }) => {
    const { tlang } = require('../lib')
  if (!isCreator) return msg.reply(tlang().owner)
  let txt = `╭───── *『 MONGODB NOTES 』* ───◆
