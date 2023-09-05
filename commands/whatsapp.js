8447eef2c41a6fdb0b993ab6828f26e86350757a
Rawmver <139946633+Rawmver@users.noreply.github.com>
2023-09-05 14:25:03 +0500

    whatsapp.js

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -44,7 +44,7 @@ CURRENTLY RUNNING ON BETA VERSION!!
  const moment = require("moment-timezone")
  const Config = require('../config')
  let { fancytext, tlang, tiny, botpic ,prefix,smd } = require("../lib");
- let sᴜʜᴀɪʟ_ᴍᴅ ;
+ let Lucifer ;
  
 const {updateProfilePicture,forwardMessage, getAdmin} = require("../lib")
 
@@ -52,42 +52,42 @@ const {updateProfilePicture,forwardMessage, getAdmin} = require("../lib")
 
 
 ///============================================================================================
-smd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+smd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",filename: __filename},async(Lucifer, msg, text,{cmdName , isCreator}) => {
    if(!msg.quoted) return await msg.send("*Uhh please, reply to a video Message*")
    let mtype = msg.quoted.mtype 
    if(mtype !== "videoMessage") return await msg.send("*Replied Message is not a video, Idiot.*")
-   return await forwardMessage(msg.chat, Suhail, msg, cmdName )
+   return await forwardMessage(msg.chat, Lucifer, msg, cmdName )
 })////------------------------------------------------------------
-smd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+smd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",filename: __filename},async(Lucifer, msg, text,{cmdName , isCreator}) => {
    if(!msg.quoted) return await msg.send("*Uhh Please, reply to whatsapp status*")
-   let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
-   return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Suhail, msg, cmdName )
+   let Lucifer_MD_num = await Lucifer.bot.decodeJid(msg.user)
+   return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Lucifer, msg, cmdName )
 })//--------------------------------------------------------------------
-smd({pattern: "save",desc: "Save Message to log number",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+smd({pattern: "save",desc: "Save Message to log number",category: "whatsapp",filename: __filename},async(Lucifer, msg, text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner)
    if(!msg.quoted) return await msg.send("*Uhh Please, reply to a Message*")
-   let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
-   return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Suhail, msg, cmdName )
+   let Lucifer_MD_num = await Lucifer.bot.decodeJid(msg.user)
+   return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Lucifer, msg, cmdName )
 })///================================================================================
-smd({ on: "text" }, async (Suhail,msg,text)=> {
+smd({ on: "text" }, async (Lucifer,msg,text)=> {
    if(msg.quoted && msg.text.toLowerCase().includes("send") ){
-       let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
+       let Lucifer_MD_num = await Suhail.bot.decodeJid(msg.user)
        if(msg.quoted.sender === sᴜʜᴀɪʟ_ᴍᴅ_num && msg.quoted.chat === 'status@broadcast' ){ return await forwardMessage(msg.chat, Suhail.bot, msg, 'send' ); }
    }
 })//==================================================================
 
 smd({pattern: "pp",desc: "Set profile picture",category: "whatsapp",use: '<reply to image>', filename: __filename,},
-async(Suhail, msg,text) => {
+async(Lucifer, msg,text) => {
    if (!msg.quoted) return await msg.reply("*Reply to an image, dear*");
    if(msg.quoted.mtype !='imageMessage') return await msg.reply("*_Uhh please, Reply to an image._*");    
-   const sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
+   const Lucifer_MD_num = await Suhail.bot.decodeJid(msg.user)
    return await updateProfilePicture(Suhail , sᴜʜᴀɪʟ_ᴍᴅ_num , msg , 'pp' );
  })
 smd({ pattern: "fullpp", desc: "Set full screen profile picture", category: "whatsapp", use: '<reply to image>', filename: __filename,},
- async(Suhail, msg,text) => {
+ async(Lucifer, msg,text) => {
    if (!msg.quoted) return await msg.reply("*Reply to an image, dear*");
    if(msg.quoted.mtype !='imageMessage') return await msg.reply("*_Uhh please, Reply to an image._*");
-   const sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
+   const Lucifer_MD_num = await Suhail.bot.decodeJid(msg.user)
    return await updateProfilePicture(Suhail , sᴜʜᴀɪʟ_ᴍᴅ_num , msg , 'fullpp' );    
 });
 
